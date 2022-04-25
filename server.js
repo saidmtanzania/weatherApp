dc096bd48cd87b06e3f758b109b5240b1e718fee
saidmtanzania <saidmtanzania@gmail.com>
2022-04-25 16:35:55 +0300

    finial changes

--- server.js
+++ server.js
@@ -7,12 +7,15 @@ const request = require('request');
 
 const app = express();
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
+const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 
 
 
 app.get("/", (req,res)=>{
-    res.sendFile(__dirname + "/index.html");
+    res.render("home",{StartContent:homeStartingContent
+    });
 });
 app.post("/", (req,res)=>{
     const city = req.body.cityName;
